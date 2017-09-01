3d945ec04f6cf47afc700a1ae023adb73056e654
duckhee <fain9301@yahoo.com>
2017-09-01 16:18:34 +0900

    fix camera controller:

--- server/controllers/camera.js
+++ server/controllers/camera.js
@@ -1,18 +1,19 @@
 var models = require('../models/index');
 var Camrea = require('../models/');
 
-
-exports.create_picture = function(req, res) {
+//camera path insert db
+exports.create_picture = function(camera_info, callback) {
     models.Camrea.create({
 
     }).then(function(row) {
-
-    }).catch(function(e) {
-
+        callback(null, row);
+    }).catch(function(err) {
+        callback(err, null);
     });
 };
 
-exports.check_create_picture = function(req, res) {
+//check path and camera path insert db 
+exports.check_create_picture = function(camera_info, callback) {
     models.Camrea.findOrCreate({
         where: {
 
@@ -21,51 +22,57 @@ exports.check_create_picture = function(req, res) {
 
         }
     }).then(function(result) {
-
-    }).catch(function(e) {
-
+        callback(null, result);
+    }).catch(function(err) {
+        callback(err, null);
     });
 };
 
-exports.find_picture = function(req, res) {
+//find image path select one
+exports.find_picture = function(camera_info, callback) {
     models.Camrea.find({
 
-    }).then(function(resultrow) {
-
-    }).catch(function(e) {
-
+    }).then(function(result) {
+        callback(null, result);
+    }).catch(function(err) {
+        callback(err, null);
     });
 };
 
-exports.list_find_picture = function(req, res) {
+//find image path all
+exports.list_find_picture = function(camera_info, callback) {
     models.Camrea.findAll({
 
     }).then(function(result) {
-
-    }).catch(function(e) {
-
+        callback(null, result);
+    }).catch(function(err) {
+        callback(err, null);
     });
 };
 
-exports.delete_picture = function(req, res) {
+//delete image path 
+exports.delete_picture = function(camera_info, callback) {
     models.Camrea.destory({
         where: ''
     }).then(function(result) {
-
-    }).catch(function(e) {
-
+        callback(null, result);
+    }).catch(function(err) {
+        callback(err, null);
     });
 };
 
-exports.modify_picture = function(req, res) {
+//modify image path
+exports.modify_picture = function(camera_info, callback) {
     models.Camrea.update({
         field_name: ''
     }, {
-        where: {},
+        where: {
+
+        },
         returning: true
     }).then(function(result) {
-
-    }).catch(function(e) {
-
+        callback(null, result);
+    }).catch(function(err) {
+        callback(err, null);
     });
 };
